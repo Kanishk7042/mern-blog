c0e57b61581d447191669aa7f413800f6ff7897a
Kanishk7042 <kanishkaswal14@gmail.com>
2024-11-05 10:46:22 +0530

    added functionality to the sign up page

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true, // Add this line
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
+
