8537ced91e90de0189189dffa3a8ee1e1f2c9c46
Kanishk7042 <kanishkaswal14@gmail.com>
2024-11-06 12:10:08 +0530

    Added dark mode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,12 +2,16 @@ import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import React from 'react'
 import { Link,useLocation } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
-import {FaMoon} from 'react-icons/fa';
+import {FaMoon,FaSun} from 'react-icons/fa';
 import {useSelector} from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { toggleTheme } from '../redux/theme/themeSlice';
 const Header = () => {
     const path=useLocation().pathname;
+    const dispatch=useDispatch();
     const {currentUser}=useSelector(state=>state.user)
-  return (
+    const {theme}=useSelector((state)=>state.theme);
+    return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
         <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Blogger</span>
@@ -23,8 +27,9 @@ const Header = () => {
             <AiOutlineSearch/>
         </Button>
         <div className='flex gap-2 md:order-2 '>
-            <Button className='w-12 h-10 hidden sm:inline ' color='gray' pill>
-                <FaMoon/>
+            <Button onClick={()=>dispatch(toggleTheme())} className='w-12 h-10 hidden sm:inline ' color='gray' pill>
+                {theme==='light'?<FaSun/>:<FaMoon/>}
+                
             </Button>
             {currentUser?(
                 <Dropdown arrowIcon={false} inline label={
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,15 @@ import { store,persistor } from './redux/store.js'
 import { Provider } from 'react-redux'
 import App from './App.jsx'
 import { PersistGate } from 'redux-persist/integration/react'
+import ThemeProvider from './components/ThemeProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
   <Provider store={store}>
+    <ThemeProvider>
     <App />
+    </ThemeProvider>
+
   </Provider>
   </PersistGate>
 );
